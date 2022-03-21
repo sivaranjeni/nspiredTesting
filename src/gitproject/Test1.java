0ace28ec26067860fe2497dde59fc08d5f72987e
veeraleah <veeraleah@localhost>
2022-03-21 11:34:14 +0200

    my first commit
    Signed-off-by: Sivaranjeni <siva.ranjeni@gmail.com>

---
+++ src/gitproject/Test1.java
@@ -0,0 +1,17 @@
+
+package gitproject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String Hungry="yes";
+		if(Hungry=="yes")
+			System.out.println("eat something...");
+		else {
+			System.out.println("do work...");
+		}
+
+	}
+
+}
